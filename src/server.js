e9eba953cab9c45138e1117310ed620e2e0f0c0d
Renato da Silva Viana <verkylen@gmail.com>
2022-12-10 21:34:08 -0300

    Games routes finished

--- src/server.js
+++ src/server.js
@@ -45,10 +45,16 @@ server.post('/categories', async (req, res) => {
 });
 
 server.get('/games', async (req, res) => {
-    const name = req.query.name.toLowerCase();
+    const name = ('name' in req.query) ? req.query.name.toLowerCase() : '';
 
     try {
-        const {rows} = await connection.query('SELECT * FROM games WHERE LOWER(name) LIKE ' + `'${name}%';`);
+        const {rows} = await connection.query(`
+            SELECT games.*, categories.name AS "categoryName"
+            FROM games JOIN categories
+            ON games."categoryId" = categories.id
+            WHERE LOWER (games.name) LIKE '${name}%';
+        `);
+
         res.send(rows);
     } catch {
         res.sendStatus(500);
@@ -58,12 +64,85 @@ server.get('/games', async (req, res) => {
 server.post('/games', async (req, res) => {
     const bodySchema = joi.object({
         name: joi.string().required(),
-        image: 'http://',
-        stockTotal: 3,
-        categoryId: 1,
-        pricePerDay: 1500,
+        image: joi.string().pattern(new RegExp('^https://')).required(),
+        stockTotal: joi.number().integer().positive().required(),
+        categoryId: joi.number().integer().positive().required(),
+        pricePerDay: joi.number().positive().required()
     });
+
+    const {body} = req;
+
+    const validation = bodySchema.validate(body, {abortEarly: true});
+
+    if ('error' in validation) {
+        res.sendStatus(400);
+        return;
+    }
+
+    const {name, image, stockTotal, categoryId, pricePerDay} = body;
+
+    try {
+        const {rows: category} = await connection.query('SELECT * FROM categories WHERE id = $1;', [categoryId]);
+
+        if (category.length === 0) {
+            res.sendStatus(400);
+            return;
+        }
+
+        const {rows: game} = await connection.query('SELECT * FROM games WHERE name = $1;', [name]);
+
+        if (game.length !== 0) {
+            res.sendStatus(409);
+            return;
+        }
+
+        await connection.query(`
+            INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay")
+            VALUES ($1, $2, $3, $4, $5);
+        `, [name, image, stockTotal, categoryId, pricePerDay]);
+
+        res.sendStatus(201);
+    } catch {
+        res.sendStatus(500);
+    }
 });
+
+server.get('/customers', async (req, res) => {
+    const {query} = req;
+
+    const cpf = ('cpf' in query) ? query.cpf : '';
+
+    try {
+        const {rows} = await connection.query(`
+            SELECT id, name, phone, cpf, TO_CHAR(birthday :: DATE, 'YYYY-MM-DD') AS birthday
+            FROM customers
+            WHERE cpf LIKE '${cpf}%';
+        `);
+
+        res.send(rows);
+    } catch {
+        res.sendStatus(500);
+    }
+});
+
+server.get('/customers/:id', async (req, res) => {
+    try {
+        const {rows} = await connection.query(`
+            SELECT id, name, phone, cpf, TO_CHAR(birthday :: DATE, 'YYYY-MM-DD') AS birthday
+            FROM customers
+            WHERE id = ${req.params.id}';
+        `);
+
+        res.send(rows);
+    } catch {
+        res.sendStatus(500);
+    }
+});
+
+
+// server.post('/customers', async (req, res) => {
+
+// });
 
 const port = 4000;
 server.listen(port, () => console.log('Express server listening on localhost:' + port));
