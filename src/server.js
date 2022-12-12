52466cf4f8c04901b189063f1cd275c3a8958dc7
Renato da Silva Viana <verkylen@gmail.com>
2022-12-12 08:02:15 -0300

    End-points implemented

--- src/server.js
+++ src/server.js
@@ -67,7 +67,7 @@ server.post('/games', async (req, res) => {
         image: joi.string().pattern(new RegExp('^https://')).required(),
         stockTotal: joi.number().integer().positive().required(),
         categoryId: joi.number().integer().positive().required(),
-        pricePerDay: joi.number().positive().required()
+        pricePerDay: joi.number().integer().positive().required(),
     });
 
     const {body} = req;
@@ -79,7 +79,20 @@ server.post('/games', async (req, res) => {
         return;
     }
 
+    const regex = new RegExp('^[0-9]+$');
+
     const {name, image, stockTotal, categoryId, pricePerDay} = body;
+
+    const disallows = [
+        regex.test(stockTotal) === false,
+        regex.test(categoryId) === false,
+        regex.test(pricePerDay) === false
+    ];
+
+    if (disallows[0] || disallows[1] || disallows[2]) {
+        res.sendStatus(400);
+        return;
+    }
 
     try {
         const {rows: category} = await connection.query('SELECT * FROM categories WHERE id = $1;', [categoryId]);
@@ -137,12 +150,21 @@ server.get('/customers/:id', async (req, res) => {
         return;
     }
 
+    const regex = new RegExp('^[0-9]+$');
+
+    const {id} = params;
+
+    if (regex.test(id) === false) {
+        res.sendStatus(404);
+        return;
+    }
+
     try {
         const {rows} = await connection.query(`
             SELECT id, name, phone, cpf, TO_CHAR(birthday :: DATE, 'YYYY-MM-DD') AS birthday
             FROM customers
             WHERE id = $1;
-        `, [params.id]);
+        `, [id]);
 
         if (rows.length === 0) {
             res.sendStatus(404);
@@ -273,7 +295,14 @@ server.put('/customers/:id', async (req, res) => {
         return;
     }
 
+    const regex = new RegExp('^[0-9]+$');
+
     const {id} = params;
+
+    if (regex.test(id) === false) {
+        res.sendStatus(404);
+        return;
+    }
 
     try {
         const {rows} = await connection.query('SELECT * FROM customers WHERE id = $1;', [id]);
@@ -293,6 +322,268 @@ server.put('/customers/:id', async (req, res) => {
     } catch {
         res.sendStatus(500);
         return;
+    }
+});
+
+server.get('/rentals', async (req, res) => {
+    const querySchema = joi.object({
+        customerId: joi.number().integer().positive(),
+        gameId: joi.number().integer().positive()
+    });
+
+    const {query} = req;
+
+    const validation = querySchema.validate(query);
+
+    if ('error' in validation) {
+        res.sendStatus(405);
+        return;
+    }
+
+    const queries = Object.entries(query);
+
+    let filter = '';
+
+    if (queries.length !== 0) {
+        const [firstSpecifiedIdKey, firstSpecifiedIdValue] = queries[0];
+
+        const regex = new RegExp('^[0-9]+$');
+    
+        if (regex.test(firstSpecifiedIdValue) === false) {
+            res.sendStatus(405);
+            return;
+        }
+            
+        if (queries.length === 1) {
+            filter = `WHERE "${firstSpecifiedIdKey}" = ${firstSpecifiedIdValue}`;
+        }
+
+        if (queries.length === 2) {
+            const [secondSpecifiedIdKey, secondSpecifiedIdValue] = queries[1];
+
+            if (regex.test(secondSpecifiedIdValue) === false) {
+                res.sendStatus(405);
+                return;
+            }
+
+            filter = `
+                WHERE "${firstSpecifiedIdKey}" = ${firstSpecifiedIdValue}
+                AND "${secondSpecifiedIdKey}" = ${secondSpecifiedIdValue}
+            `;
+        }
+    }
+    
+    try {
+        const {rows} = await connection.query(`
+            SELECT rentals.*, TO_JSON(c) AS customer, TO_JSON(g) AS game
+            FROM rentals JOIN (SELECT id, name FROM customers) c
+            ON rentals."customerId" = c.id
+            JOIN (
+                SELECT games.id, games.name, games."categoryId", categories.name AS "categoryName"
+                FROM games JOIN categories
+                ON games."categoryId" = categories.id
+            ) g
+            ON rentals."gameId" = g.id ${filter};
+        `);
+
+        res.send(rows);
+    } catch {
+        res.sendStatus(500);
+    }
+});
+
+server.post('/rentals', async (req, res) => {
+    const schema = joi.number().integer().positive().required();
+
+    const bodySchema = joi.object({
+        customerId: schema,
+        gameId: schema,
+        daysRented: schema
+    });
+
+    const {body} = req;
+
+    const validation = bodySchema.validate(body, {abortEarly: true});
+
+    if ('error' in validation) {
+        res.sendStatus(400);
+        return;
+    }
+
+    const regex = new RegExp('^[0-9]+$');
+
+    const {customerId, gameId, daysRented} = body;
+
+    const disallows = [
+        regex.test(customerId) === false,
+        regex.test(gameId) === false,
+        regex.test(daysRented) === false
+    ];
+
+    if (disallows[0] || disallows[1] || disallows[2]) {
+        res.sendStatus(400);
+        return;
+    }
+
+    try {
+        const {rows: customer} = await connection.query('SELECT * FROM customers WHERE id = $1;', [customerId]);
+
+        if (customer.length === 0) {
+            res.sendStatus(400);
+            return;
+        }
+
+        const {rows: game} = await connection.query('SELECT * FROM games WHERE id = $1;', [gameId]);
+
+        if (game.length === 0) {
+            res.sendStatus(400);
+            return;
+        }
+
+        const {stockTotal, pricePerDay} = game[0];
+
+        const {rows: rentals} = await connection.query('SELECT * FROM rentals WHERE "gameId" = $1;', [gameId]);
+
+        let outOfStock = 0;
+
+        for (const rental of rentals) {
+            if (rental.returnDate === null) outOfStock++;
+        }
+
+        if (outOfStock >= stockTotal) {
+            res.sendStatus(400);
+            return;
+        }
+        
+        const nowDate = new Date();
+
+        const date = nowDate.getDate().toString().padStart(2, 0);
+        const month = (nowDate.getMonth() + 1).toString().padStart(2, 0);
+        const year = nowDate.getFullYear();
+
+        const rentDate = `${year}-${month}-${date}`;
+
+        const originalPrice = Number(daysRented) * Number(pricePerDay);
+
+        await connection.query(`
+            INSERT INTO rentals (
+                "customerId",
+                "gameId",
+                "rentDate",
+                "daysRented",
+                "returnDate",
+                "originalPrice",
+                "delayFee"
+            ) VALUES ($1, $2, $3, $4, $5, $6, $7);
+        `, [customerId, gameId, rentDate, daysRented, null, originalPrice, null]);
+
+        res.sendStatus(201);
+    } catch {
+        res.sendStatus(500);
+    }
+});
+
+server.post('/rentals/:id/return', async (req, res) => {
+    const paramsSchema = joi.number().integer().positive();
+
+    const {id} = req.params;
+
+    const validation = paramsSchema.validate(id);
+
+    if ('error' in validation) {
+        res.sendStatus(404);
+        return;
+    }
+
+    const regex = new RegExp('^[0-9]+$');
+
+    if (regex.test(id) === false) {
+        res.sendStatus(404);
+        return;
+    }
+
+    try {
+        const {rows} = await connection.query(`
+            SELECT rentals.*, games."pricePerDay" AS "gamePricePerDay"
+            FROM rentals JOIN games
+            ON rentals."gameId" = games.id
+            WHERE rentals.id = $1;
+        `, [id]);
+
+        if (rows.length === 0) {
+            res.sendStatus(404);
+            return;
+        }
+
+        if (rows[0].returnDate !== null) {
+            res.sendStatus(400);
+            return;
+        }
+
+        const nowDate = new Date();
+
+        const dayMilliseconds = 24 * 60 * 60 * 1000;
+
+        const {rentDate, daysRented, gamePricePerDay} = rows[0];
+
+        const millisecondsRented = daysRented * dayMilliseconds;
+
+        let delayFee = null;
+
+        const millisecondsDifference = (nowDate - dayMilliseconds) - rentDate;
+
+        if (millisecondsDifference > millisecondsRented) {
+            const delayDays = Math.floor(millisecondsDifference / dayMilliseconds);
+
+            delayFee = delayDays * Number(gamePricePerDay);
+        }
+
+        const date = nowDate.getDate().toString().padStart(2, 0);
+        const month = (nowDate.getMonth() + 1).toString().padStart(2, 0);
+        const year = nowDate.getFullYear();
+    
+        const returnDate = `${year}-${month}-${date}`;
+
+        await connection.query(`
+            UPDATE rentals SET "returnDate" = $1, "delayFee" = $2 WHERE id = $3;
+        `, [returnDate, delayFee, id]);
+
+        res.sendStatus(200);
+    } catch {
+        res.sendStatus(500);
+    }
+});
+
+server.delete('/rentals/:id', async (req, res) => {
+    const paramsSchema = joi.string().pattern(new RegExp('^[0-9]+$'));
+
+    const {id} = req.params;
+
+    const validation = paramsSchema.validate(id);
+
+    if ('error' in validation) {
+        res.sendStatus(404);
+        return;
+    }
+
+    try {
+        const {rows} = await connection.query('SELECT * FROM rentals WHERE id = $1;', [id]);
+
+        if (rows.length === 0) {
+            res.sendStatus(404);
+            return;
+        }
+
+        if (rows[0].returnDate === null) {
+            res.sendStatus(400);
+            return;
+        }
+
+        await connection.query('DELETE FROM rentals WHERE id = $1;', [id]);
+
+        res.sendStatus(200);
+    } catch {
+        res.sendStatus(500);
     }
 });
 
